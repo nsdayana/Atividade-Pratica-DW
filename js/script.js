63441fd2fbfec898089ca269025b784b783a151f
nsdayana <nsdayana@hotmail.com>
2023-10-23 22:17:10 -0300

    comentarios

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
+// Recebe como parâmetro um elemento HTML
 function ativaletra(elemento) {
+
+    // Transforma o conteúdo do elemento em um array de letras
     const arrTexto = elemento.innerHTML.split('');
+
     elemento.innerHTML = '';
-    arrTexto.forEach((letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += letra;
-        }, 75 * i)
+
+    arrTexto.forEach((letra, i) => {
+        
+    // Define um timeout para adicionar a letra ao elemento com um intervalo de 75ms multiplicado pelo índice atual
+    setTimeout(() => {
+    elemento.innerHTML += letra;
+    }, 75 * i);
     });
-
-}
-
-
-const titulo = document.querySelector('.digitando');
-ativaletra(titulo);
+    }
+    
+    // Seleciona o elemento com a classe "digitando"
+    const titulo = document.querySelector('.digitando');
+    
+    // Chama a função para animar a digitação no elemento
+    ativaletra(titulo);
